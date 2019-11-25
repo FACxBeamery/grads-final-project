fc979a7442bbdc64b5a06c1fedb16b49e20ec6a8
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-25 17:09:42 +0000

    create initial pages for routes

---
+++ client/src/pages/CreateSurvey/CreateSurvey.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateSurvey = () => {
+    return <></>;
+};
+
+export default CreateSurvey;
---
+++ client/src/pages/TakeSurvey/TakeSurvey.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TakeSurvey = () => {
+    return <></>;
+};
+
+export default TakeSurvey;
