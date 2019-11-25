f5ef18fad8fbed2c82951ba2d5153190c38c84f3
thomas-galligan <tom.galligan@beamery.com>
2019-11-25 16:46:15 +0000

    initialise backend file structure

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,20 +4,12 @@ module.exports = {
         es6: true,
         node: true
     },
-    extends: "eslint:recommended",
+    extends: ["airbnb", "airbnb/hooks", "prettier"],
     globals: {
         Atomics: "readonly",
         SharedArrayBuffer: "readonly"
     },
     parserOptions: {
         ecmaVersion: 2018
-    },
-    rules: {
-        indent: ["error", 4],
-        "max-len": ["error", { code: 80 }],
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "double"],
-        semi: ["error", "always"],
-        "no-multiple-empty-lines": [2, { max: 2, maxEOF: 1 }]
     }
 };
