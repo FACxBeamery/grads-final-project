9300b72a369648299ad4c5d40d0612d3b24b09a5
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-26 11:12:49 +0000

    linting rules changes to recognize "document"

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,3 +1,10 @@
 module.exports = {
   extends: ['airbnb', 'airbnb/hooks', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+  },
+  env: {
+    browser: true,
+    jest: true,
+  },
 };
