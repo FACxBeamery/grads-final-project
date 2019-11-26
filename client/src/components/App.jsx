ef2895bd662b9aeab0697e88f3dadc2ca49af19b
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-26 11:14:01 +0000

    rename for eslint

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './Main';
+import Header from './Header';
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+};
+
+export default App;
