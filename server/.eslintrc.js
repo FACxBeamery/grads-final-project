7117f0e9ff4725ce78ed6c31ec5545294537c052
thomas-galligan <tom.galligan@beamery.com>
2019-11-26 10:52:36 +0000

    add eslint and prettier settings. Run npm i in server after pulling

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,15 +1,3 @@
 module.exports = {
-    env: {
-        commonjs: true,
-        es6: true,
-        node: true
-    },
-    extends: ["airbnb", "airbnb/hooks", "prettier"],
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly"
-    },
-    parserOptions: {
-        ecmaVersion: 2018
-    }
+    extends: ['airbnb', 'airbnb/hooks', 'prettier'],
 };
