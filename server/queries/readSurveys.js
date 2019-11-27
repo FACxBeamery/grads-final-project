ecce954dcbba76645560731e4f5cd6b3370d1c67
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-27 15:09:19 +0000

    get db collection surveys and return result in array format

---
+++ server/queries/readSurveys.js
@@ -0,0 +1,13 @@
+const { getDb } = require('../databaseConnection');
+const readUsers = async () => {
+  try {
+    const db = getDb();
+    const surveys = db.collection('Surveys');
+    const result = await surveys.find({}).toArray();
+    return result;
+  } catch (err) {
+    return err;
+  }
+};
+
+module.exports = readUsers;
