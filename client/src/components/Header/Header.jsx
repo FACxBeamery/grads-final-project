77cad3a4b2d18c019c923dd7065653faeee052c9
lyndseysc <lyndsey.736@gmail.com>
2019-11-27 09:54:42 +0000

    add reuasable header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
-
+import AppBar from '@material-ui/core/AppBar';
+import styles from './Header.module.css';
+import Typography from '@material-ui/core/Typography';
 // TODO make header component
 
 const Header = () => {
-  return <></>;
+  return (
+    <AppBar className={styles['header-bar']}>
+      <div className={styles['header-title']}>
+        <h2 className={styles['header-title-vibe']}>Vibe@</h2>
+        {/* <Typography >
+          <h2>Vibe@</h2>
+        </Typography> */}
+        <h2 className={styles['header-title-beamery']}> Beamery</h2>
+        {/* <Typography
+          color='secondary'
+         
+          variant='h4'
+        >
+       
+        </Typography> */}
+      </div>
+    </AppBar>
+  );
 };
 
 export default Header;
