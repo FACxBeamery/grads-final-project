5ac712c40375aa86379ca457e96cc53c5844bce1
lyndseysc <lyndsey.736@gmail.com>
2019-11-27 14:32:35 +0000

    render stepper on take survey page

--- client/src/pages/TakeSurvey/index.jsx
+++ client/src/pages/TakeSurvey/index.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React from 'react';
+import UserProgressStepper from './UserProgressStepper/UserProgressStepper';
 
 const TakeSurvey = () => {
-    return <></>;
+  return (
+    <>
+      {/* <p> Hello I am a fake paragrpah</p> */}
+      <UserProgressStepper></UserProgressStepper>
+    </>
+  );
 };
 
 export default TakeSurvey;
