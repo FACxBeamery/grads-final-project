36b98ab87f1ffba8c6649471a468e94e833d0a66
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-27 17:39:41 +0000

    fix uneccessary ternary operators and start styling

--- client/src/pages/CreateSurvey/index.jsx
+++ client/src/pages/CreateSurvey/index.jsx
@@ -6,6 +6,8 @@ import {
   Switch,
   FormControl,
   InputLabel,
+  Box,
+  FormControlLabel,
 } from '@material-ui/core';
 import QuestionsList from './Questions/Questions';
 
@@ -23,68 +25,75 @@ const CreateSurvey = () => {
   const handleSubmit = () => {};
   return (
     <form onSubmit={handleSubmit}>
-      <TextField
-        required
-        error={title.length < 5 || title.length > 100}
-        helperText={
-          title < 5 || title > 100
-            ? 'Title must be between 5 and 100 characters!'
-            : ''
-        }
-        value={title}
-        name='title'
-        label='Survey title'
-        onChange={setMetadata}
-      />
-      <TextField
-        required
-        error={description.length < 5 || description.length > 140}
-        helperText={
-          description < 5 || description > 140
-            ? 'Description must be between 5 and 140 characters!'
-            : ''
-        }
-        value={description}
-        name='description'
-        label='Enter a description'
-        onChange={setMetadata}
-      />
-      <TextField
-        required
-        value={recipients[0]}
-        label='PLACEHOLDER FOR RECIPIENTS'
-        name='recipients'
-        onChange={setMetadata}
-      />
-      <TextField
-        required
-        error={disclaimer.length < 5 || disclaimer.length > 140}
-        helperText={
-          disclaimer < 5 || disclaimer > 140
-            ? 'You must provide a disclaimer'
-            : ''
-        }
-        value={disclaimer}
-        name='disclaimer'
-        label='How will this data be used?'
-        onChange={setMetadata}
-      />
-      <FormControl>
-        <InputLabel id='anonymous-switch'>Anonymous</InputLabel>
-
-        <Switch
-          checked={anonymous}
-          onChange={(e) => setMetadata(e, 'switch')}
-          value='anonymous'
-          name='anonymous'
-          labelId='anonymous-switch'
-          inputProps={{ 'aria-label': 'Make survey anonymous' }}
+      <Box display='flex' flexDirection='column' my={8}>
+        <TextField
+          margin='normal'
+          required
+          error={title.length < 5 || title.length > 100}
+          helperText={
+            title < 5 ||
+            (title > 100 && 'Title must be between 5 and 100 characters!')
+          }
+          value={title}
+          name='title'
+          label='Survey title'
+          onChange={setMetadata}
         />
-      </FormControl>
-      <QuestionsList />
-      <Button type='submit' color='secondary'>
-        Create Survey
-      </Button>
+        <TextField
+          margin='normal'
+          required
+          error={description.length < 5 || description.length > 140}
+          helperText={
+            description < 5 || description > 140
+              ? 'Description must be between 5 and 140 characters!'
+              : ''
+          }
+          value={description}
+          name='description'
+          label='Enter a description'
+          onChange={setMetadata}
+        />
+        <TextField
+          required
+          value={recipients[0]}
+          label='PLACEHOLDER FOR RECIPIENTS'
+          name='recipients'
+          onChange={setMetadata}
+        />
+        <TextField
+          margin='normal'
+          required
+          error={disclaimer.length < 5 || disclaimer.length > 140}
+          helperText={
+            disclaimer < 5 || disclaimer > 140
+              ? 'You must provide a disclaimer'
+              : ''
+          }
+          value={disclaimer}
+          name='disclaimer'
+          label='How will this data be used?'
+          onChange={setMetadata}
+        />
+        <FormControlLabel
+          control={(
+<Switch
+              checked={anonymous}
+              onChange={(e) => setMetadata(e, 'switch')}
+              value='anonymous'
+              name='anonymous'
+              labelid='anonymous-switch'
+              inputProps={{ 'aria-label': 'Make survey anonymous' }}
+            />
+)}
+          label='Anonymous'
+        />
+        <QuestionsList />
+        <Box alignSelf='center' mt={8}>
+          <Button type='submit' variant='contained' color='secondary'>
+            Create Survey
+          </Button>
+        </Box>
+      </Box>
     </form>
   );
 };
