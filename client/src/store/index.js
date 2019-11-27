53cae40266ca03159e14d9a39c9a5e5f772dc6ef
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-27 09:58:35 +0000

    add createsurveyreducer to store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers } from 'redux';
 import reducer from './reducer/reducer';
+import createSurveyReducer from './reducer/createSurveyReducer';
 
-const store = createStore(combineReducers({ reducer }));
+const store = createStore(combineReducers({ reducer, createSurveyReducer }));
 
 export default store;
