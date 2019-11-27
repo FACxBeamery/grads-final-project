a42a146e9edeb8ef7687fffdab2d90ab4eb09784
lyndseysc <lyndsey.736@gmail.com>
2019-11-27 11:36:08 +0000

    move theme into seperate file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,50 +1,10 @@
 import React from 'react';
-import { createMuiTheme } from '@material-ui/core/styles';
+import theme from './theme';
 import { ThemeProvider, Button } from '@material-ui/core';
 import store from './store';
 import { Provider } from 'react-redux';
 import Main from './components/Main/Main';
 import Header from './components/Header/Header';
-
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      // light: '#FFFFFF',
-      main: '#201E5A',
-      // dark: '#201E5A',
-    },
-    secondary: {
-      main: '#F15852',
-    },
-    active: {
-      backgroundColor: '#69BE28',
-    },
-    // to use active use style={theme.palette.active}
-  },
-  typography: {
-    h1: {
-      fontFamily: 'Merriweather',
-    },
-    h2: {
-      fontFamily: 'Merriweather Sans',
-    },
-    h3: {
-      fontFamily: 'Merriweather Sans',
-    },
-    h4: {
-      fontFamily: 'Merriweather Sans',
-    },
-    button: {
-      fontFamily: 'Merriweather Sans',
-    },
-    p: {
-      fontFamily: 'Merriweather Sans',
-    },
-    span: {
-      fontFamily: 'Merriweather Sans',
-    },
-  },
-});
 
 const App = () => {
   console.log('STORE', store);
