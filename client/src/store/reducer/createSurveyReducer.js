4a2818b3b8cfbfded39c30a9ede78878961758e1
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-11-27 14:07:02 +0000

    make reducer for create survey states

--- client/src/store/reducer/createSurveyReducer.js
+++ client/src/store/reducer/createSurveyReducer.js
@@ -7,16 +7,76 @@ const initalState = {
   questions: [],
 };
 
-// setInputs({
-// ...state.inputs,
-// [e.target.name]: e.target.value
-// })
+const changeOptionText = (options, payload) => {
+  return options.map((option, index) =>
+    index === payload.optionIndex ? { ...option, text: payload.text } : option,
+  );
+};
+
+const deleteOption = (options, payload) => {
+  return options.filter((option, index) => index !== payload.optionIndex);
+};
+
+const mappedQuestions = (questions, payload, editOptions) => {
+  return questions.map((question, index) =>
+    index === payload.questionIndex
+      ? { ...question, options: editOptions(question.options, payload) }
+      : question,
+  );
+};
 
 const createSurverReducer = (state = initalState, action) => {
   const { payload } = action;
+  const defaultQuestion = {
+    title: '',
+    type: 'text',
+    required: false,
+    commentsEnabled: false,
+    options: [],
+  };
+
+  const defaultOption = {
+    text: '',
+  };
+
   switch (action.type) {
     case 'SET_METADATA':
-      return { ...state, payload };
+      return { ...state, ...payload };
+    case 'NEW_QUESTION':
+      return { ...state, questions: [...state.questions, defaultQuestion] };
+    case 'SET_QUESTION_DATA':
+      return {
+        ...state,
+        questions: state.questions.map((question, index) =>
+          index === payload.index ? { ...question, ...payload } : question,
+        ),
+      };
+    case 'DELETE_QUESTION':
+      return {
+        ...state,
+        questions: state.questions.filter(
+          (question, index) => index !== payload.index,
+        ),
+      };
+    case 'NEW_OPTION':
+      return {
+        ...state,
+        questions: state.questions.map((question, index) =>
+          index === payload.index
+            ? { ...question, options: [...question.options, defaultOption] }
+            : question,
+        ),
+      };
+    case 'SET_OPTION_DATA':
+      return {
+        ...state,
+        questions: mappedQuestions(state.questions, payload, changeOptionText),
+      };
+    case 'DELETE_OPTION':
+      return {
+        ...state,
+        questions: mappedQuestions(state.questions, payload, deleteOption),
+      };
     default:
       return state;
   }
