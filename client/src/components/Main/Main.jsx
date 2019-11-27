85e4a44210506f0bcd5e0a7364a4867cfff2787d
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-27 15:07:14 +0000

    margin left and right on main content

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import AdminLogin from '../../pages/AdminLogin';
-import Dashboard from '../../pages/Dashboard';
+import Dashboard from '../../pages/Dashboard/Dashboard';
 import CreateSurvey from '../../pages/CreateSurvey';
 import SurveyDetail from '../../pages/SurveyDetail';
 import TakeSurvey from '../../pages/TakeSurvey';
+import Box from '@material-ui/core/Box';
 
 const Main = () => {
   return (
     <main>
-      <Switch>
-        <Route exact path='/admin/login' component={AdminLogin} />
-        <Route exact path='/admin' component={Dashboard} />
-        <Route exact path='/admin/surveys/create' component={CreateSurvey} />
-        <Route path='/admin/surveys/:id' component={SurveyDetail} />
-        <Route path='/:id' component={TakeSurvey} />
-      </Switch>
+      <Box mx={4}>
+        <Switch>
+          <Route exact path='/admin/login' component={AdminLogin} />
+          <Route exact path='/admin' component={Dashboard} />
+          <Route exact path='/admin/surveys/create' component={CreateSurvey} />
+          <Route path='/admin/surveys/:id' component={SurveyDetail} />
+          <Route path='/:id' component={TakeSurvey} />
+        </Switch>
+      </Box>
     </main>
   );
 };
