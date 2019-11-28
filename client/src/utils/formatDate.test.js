5c0cd925daa19a3c279baccdb72f5b8481b89e66
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-28 16:38:04 +0000

    change output from testing

--- client/src/utils/formatDate.test.js
+++ client/src/utils/formatDate.test.js
@@ -4,7 +4,7 @@ describe('date should format unix time to dd/mm/yyyy ', () => {
   it('should format date', () => {
     expect(formatDate(123123123)).toEqual('26/11/1973');
     expect(formatDate('123123123')).toEqual('26/11/1973');
-    expect(formatDate('')).toEqual(null);
-    expect(formatDate(null)).toEqual(null);
+    expect(formatDate('')).toEqual(`input is not unix`);
+    expect(formatDate(null)).toEqual(`input is not unix`);
   });
 });
