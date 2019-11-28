b5f3462359d555dcae8121398d4c2eb0a0458e5e
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-28 15:00:02 +0000

    clicking on view redirects to survey detail path

--- client/src/pages/Dashboard/components/SurveyCard.jsx
+++ client/src/pages/Dashboard/components/SurveyCard.jsx
@@ -1,56 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Box,
+  Chip,
+  Grid,
+  Typography,
+} from '@material-ui/core';
+
 import ProgressWheel from '../../../components/ProgressWheel/ProgressWheel';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
-import Chip from '@material-ui/core/Chip';
-import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
-import styles from '../Dashboard.module.css';
-import Typography from '@material-ui/core/Typography';
+
+import styles from './SurveyCard.module.css';
 
 const SurveyCard = ({ survey }) => {
-  const {
-    _id,
-    title,
-    description,
-    status,
-    datePublished,
-    dateToClose,
-    recipients,
-    responses,
-  } = survey;
+  const { _id, title, description, recipients, responses } = survey;
   const percentage = Math.floor((responses.length / recipients.length) * 100);
   return (
     <Grid item xs={5} component={Card} className={styles.card}>
-      {/* <Card> */}
-      <CardContent>
-        <Typography color='textSecondary' gutterBottom variant='subtitle2'>
-          SURVEY
-        </Typography>
-        <Box container display={'flex'} justifyContent='space-between' mb={4}>
-          <Typography variant='h6'>{title}</Typography>
-
-          <ProgressWheel
-            strokeWidth={'8'}
-            sqSize={'80'}
-            percentage={percentage || 0}
-            numerator={responses.length}
-            denominator={recipients.length}
-          />
-        </Box>
-        <Typography color='textSecondary' variant='body2'>
-          {description}
-        </Typography>
-      </CardContent>
-      <CardActions className={styles['actions']}>
-        <Button color='secondary' size='small'>
-          View
-        </Button>
-        <Chip label='Active' className={styles['active']} />
-      </CardActions>
+      <Box
+        display={'flex'}
+        flexDirection='column'
+        justifyContent='space-between'
+      >
+        <CardContent>
+          <Typography color='textSecondary' gutterBottom variant='subtitle2'>
+            SURVEY
+          </Typography>
+          <Box display={'flex'} mb={1}>
+            <Box display={'flex'} flexDirection='column'>
+              <Typography variant='h6'>{title}</Typography>
+              <Typography color='textSecondary' variant='body2'>
+                {description}
+              </Typography>
+            </Box>
+            <Box display={'flex'}>
+              <ProgressWheel
+                strokeWidth={'10'}
+                sqSize={'100'}
+                percentage={percentage || 0}
+                numerator={responses.length}
+                denominator={recipients.length}
+              />
+            </Box>
+          </Box>
+        </CardContent>
+        <CardActions className={styles['actions']}>
+          <Button color='secondary' size='small'>
+            <Link
+              className={styles.link}
+              to={{
+                pathname: `admin/surveys/${_id}`,
+              }}
+            >
+              View
+            </Link>
+          </Button>
+          <Chip label='Active' className={styles['active']} />
+        </CardActions>
+      </Box>
     </Grid>
   );
 };
