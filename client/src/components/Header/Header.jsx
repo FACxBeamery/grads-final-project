b83d40e7f7d3b60603f9dfe4721f4437d2d20d05
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-28 16:28:23 +0000

    appbar is static.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
+import { AppBar, Box } from '@material-ui/core';
 import styles from './Header.module.css';
-import Box from '@material-ui/core/Box';
-//import Typography from '@material-ui/core/Typography';
 // TODO make header component
 
 const Header = () => {
   return (
-    <AppBar data-testid='app-bar' className={styles['header-bar']}>
+    <AppBar data-testid='app-bar' position='static'>
       <Box className={styles['header-title']}>
         <h2 className={styles['header-title-vibe']}>Vibe@</h2>
-
         <h2 className={styles['header-title-beamery']}> Beamery</h2>
       </Box>
     </AppBar>
