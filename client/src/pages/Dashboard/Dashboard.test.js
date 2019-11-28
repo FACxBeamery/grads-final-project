bb5abfded854cbaea588042abdaac19cf48f7dfd
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-28 15:37:12 +0000

    test if dashboard works as expect - integration testing

---
+++ client/src/pages/Dashboard/Dashboard.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import configureStore from 'redux-mock-store';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Dashboard from './index.jsx';
+
+const mockStore = configureStore([]);
+
+describe('Testing Dashboard', () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      dashboardReducer: {
+        surveys: [],
+      },
+    });
+  });
+
+  it('should alternate between two button texts', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Dashboard />
+        </Provider>
+      </BrowserRouter>,
+    );
+    expect(getByText('See all Surveys')).toBeInTheDocument();
+    fireEvent.click(getByText('See all Surveys'));
+    expect(getByText('See only Active Surveys')).toBeInTheDocument();
+  });
+  it('should alternate between two title texts', () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Dashboard />
+        </Provider>
+      </BrowserRouter>,
+    );
+    expect(getByText('Active Surveys')).toBeInTheDocument();
+    fireEvent.click(getByText('See all Surveys'));
+    expect(getByText('All Surveys')).toBeInTheDocument();
+  });
+});
