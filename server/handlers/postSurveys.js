195f308793c1096f370b3b0727a69f5245e6f057
thomas-galligan <tom.galligan@beamery.com>
2019-11-28 10:36:34 +0000

    fix typo

--- server/handlers/postSurveys.js
+++ server/handlers/postSurveys.js
@@ -29,40 +29,39 @@ const Joi = require('joi');
 const postSurveys = async (req, res) => {
   console.log('postSurveys handler running');
 
-  VALIDATION AVOIDED FOR NOW TO MAKE TESTING EASIER
-    const surveyObjectSchema = Joi.object().keys({
-      title: Joi.string()
-        .min(5)
-        .max(100)
-        .required(),
-      description: Joi.string()
-        .min(5)
-        .max(140)
-        .required(),
-      status: Joi.string().valid('created', 'published', 'closed'),
-      dateCreated: Joi.string(),
-      dateToPublish: Joi.string(),
-      datePublished: Joi.string(),
-      dateToClose: Joi.string(),
-      dateClosed: Joi.string(),
-      anonymous: Joi.boolean(),
-      recipients: Joi.array().items(
-        Joi.object().keys({ employeeId: Joi.string(), completed: Joi.boolean() }),
-      ),
-      questions: Joi.array().items(
-        Joi.object().keys({ position: Joi.number().integer() }),
-      ),
-      responses: Joi.array().items(
-        Joi.object().keys({
-          answers: Joi.array().items(
-            Joi.object().keys({
-              answer: Joi.alternatives().try(Joi.number(), Joi.string()),
-              comment: Joi.string().allow(null),
-            }),
-          ),
-        }),
-      ),
-    });
+  const surveyObjectSchema = Joi.object().keys({
+    title: Joi.string()
+      .min(5)
+      .max(100)
+      .required(),
+    description: Joi.string()
+      .min(5)
+      .max(140)
+      .required(),
+    status: Joi.string().valid('created', 'published', 'closed'),
+    dateCreated: Joi.string(),
+    dateToPublish: Joi.string(),
+    datePublished: Joi.string(),
+    dateToClose: Joi.string(),
+    dateClosed: Joi.string(),
+    anonymous: Joi.boolean(),
+    recipients: Joi.array().items(
+      Joi.object().keys({ employeeId: Joi.string(), completed: Joi.boolean() }),
+    ),
+    questions: Joi.array().items(
+      Joi.object().keys({ position: Joi.number().integer() }),
+    ),
+    responses: Joi.array().items(
+      Joi.object().keys({
+        answers: Joi.array().items(
+          Joi.object().keys({
+            answer: Joi.alternatives().try(Joi.number(), Joi.string()),
+            comment: Joi.string().allow(null),
+          }),
+        ),
+      }),
+    ),
+  });
   const surveyObj = req.body;
   console.log(surveyObj, typeof surveyObj);
   const questions = surveyObj.questions;
