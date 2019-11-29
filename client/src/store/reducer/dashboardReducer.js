1bc972b964fcf56f2927d8f9f233d5a39fed0e5f
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-29 10:39:31 +0000

    swap_views action type

--- client/src/store/reducer/dashboardReducer.js
+++ client/src/store/reducer/dashboardReducer.js
@@ -1,9 +1,12 @@
 const initalState = {
   surveys: [],
+  showActiveSurveys: true,
 };
 
 const dashboardReducer = (state = initalState, action) => {
   switch (action.type) {
+    case 'SWAP_VIEWS':
+      return { ...state, showActiveSurveys: !state.showActiveSurveys };
     case 'SET_SURVEYS':
       return { ...state, surveys: action.payload };
     default:
