5d5b932ef1fd4dc74772b4202fe3063c9e1a351d
lyndseysc <lyndsey.736@gmail.com>
2019-11-29 09:48:48 +0000

    progress stepper

--- client/src/pages/TakeSurvey/UserProgressStepper/UserProgressStepper.jsx
+++ client/src/pages/TakeSurvey/UserProgressStepper/UserProgressStepper.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import MobileStepper from '@material-ui/core/MobileStepper';
 import { useSelector, useDispatch } from 'react-redux';
-import Button from '@material-ui/core/Button';
+import { Button, Box } from '@material-ui/core/';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import styles from './UserProgressStepper.module.css';
 
 const UserProgressStepper = () => {
   const dispatch = useDispatch();
   const activeStep = useSelector((state) => state.takeSurveyReducer.activeStep);
   const questions = useSelector((state) => state.takeSurveyReducer.questions);
-  // const QuestionsArray = [1, 2, 3, 4, 5, 6];
-  const numberOfSteps = questions.length + 1;
+
+  const numberOfSteps = questions.length + 2;
 
   const nextQuestion = () => {
     dispatch({ type: 'NEXT_STEP' });
@@ -29,27 +28,32 @@ const UserProgressStepper = () => {
   };
 
   return (
-    <MobileStepper
-      className={styles['mobile-stepper']}
-      variant='progress'
-      position='static'
-      activeStep={activeStep}
-      steps={numberOfSteps}
-      nextButton={
-        <Button size='small' onClick={nextQuestion} disabled={activeStep === 5}>
-          <KeyboardArrowRight />
-        </Button>
-      }
-      backButton={
-        <Button
-          size='small'
-          onClick={previousQuestion}
-          disabled={activeStep === 0}
-        >
-          <KeyboardArrowLeft />
-        </Button>
-      }
-    ></MobileStepper>
+    <Box>
+      <MobileStepper
+        variant='progress'
+        position='static'
+        activeStep={activeStep}
+        steps={numberOfSteps}
+        nextButton={
+          <Button
+            size='small'
+            onClick={nextQuestion}
+            disabled={activeStep === 5}
+          >
+            <KeyboardArrowRight />
+          </Button>
+        }
+        backButton={
+          <Button
+            size='small'
+            onClick={previousQuestion}
+            disabled={activeStep === 0}
+          >
+            <KeyboardArrowLeft />
+          </Button>
+        }
+      ></MobileStepper>
+    </Box>
   );
 };
 
