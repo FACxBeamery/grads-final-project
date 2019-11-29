977c17c28bba82aff72f5f2d5eda55921dabcdad
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-11-29 10:38:53 +0000

    wrap table in Paper ui component

--- client/src/pages/Dashboard/components/AllSurveysTable.jsx
+++ client/src/pages/Dashboard/components/AllSurveysTable.jsx
@@ -7,6 +7,7 @@ import {
   TableCell,
   TableHead,
   TableRow,
+  Paper,
 } from '@material-ui/core';
 
 import styles from './AllSurveysTable.module.css';
@@ -22,42 +23,44 @@ const AllSurveysTable = ({ surveys, history }) => {
     'Status',
   ];
   return (
-    <Table aria-label='all surveys table' className={styles.table}>
-      <TableHead>
-        <TableRow>
-          {cells.map((cell, idx) => {
-            // eslint-disable-next-line react/no-array-index-key
-            return <TableCell key={idx}>{cell}</TableCell>;
+    <Paper>
+      <Table aria-label='all surveys table' className={styles.table}>
+        <TableHead>
+          <TableRow>
+            {cells.map((cell, idx) => {
+              // eslint-disable-next-line react/no-array-index-key
+              return <TableCell key={idx}>{cell}</TableCell>;
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {surveys.map((survey) => {
+            const {
+              _id,
+              title,
+              status,
+              datePublished,
+              dateToClose,
+              recipients,
+              responses,
+            } = survey;
+            return (
+              <TableRow
+                key={title}
+                className={styles.row}
+                onClick={() => history.push(`admin/surveys/${_id}`)}
+              >
+                <TableCell scope='row'>{title}</TableCell>
+                <TableCell>{`${responses.length}/${recipients.length} respondents`}</TableCell>
+                <TableCell>{formatDate(datePublished) || 'No date'}</TableCell>
+                <TableCell>{formatDate(dateToClose) || 'No date'}</TableCell>
+                <TableCell className={styles[status]}>{status}</TableCell>
+              </TableRow>
+            );
           })}
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {surveys.map((survey) => {
-          const {
-            _id,
-            title,
-            status,
-            datePublished,
-            dateToClose,
-            recipients,
-            responses,
-          } = survey;
-          return (
-            <TableRow
-              key={title}
-              className={styles.row}
-              onClick={() => history.push(`admin/surveys/${_id}`)}
-            >
-              <TableCell scope='row'>{title}</TableCell>
-              <TableCell>{`${responses.length}/${recipients.length} respondents`}</TableCell>
-              <TableCell>{formatDate(datePublished) || 'No date'}</TableCell>
-              <TableCell>{formatDate(dateToClose) || 'No date'}</TableCell>
-              <TableCell className={styles[status]}>{status}</TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+        </TableBody>
+      </Table>
+    </Paper>
   );
 };
 
