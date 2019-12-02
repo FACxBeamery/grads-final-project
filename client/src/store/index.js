995f2c1b347ff0e6cc6a7d570e84eb982ef486ae
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-12-02 16:50:33 +0000

    use new reducer in store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,9 +3,14 @@ import { createStore, combineReducers } from 'redux';
 import reducer from './reducer/reducer';
 import createSurveyReducer from './reducer/createSurveyReducer';
 import dashboardReducer from './reducer/dashboardReducer';
-
+import employeeTableReducer from './reducer/employeeTableReducer';
 const store = createStore(
-  combineReducers({ reducer, createSurveyReducer, dashboardReducer }),
+  combineReducers({
+    reducer,
+    createSurveyReducer,
+    dashboardReducer,
+    employeeTableReducer,
+  }),
 );
 
 export default store;
