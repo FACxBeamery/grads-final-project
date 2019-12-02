15fb4df3068adea8743790ceb461575b66ed6671
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-12-02 14:09:55 +0000

    fix date format

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -1,6 +1,6 @@
 const formatDate = (unixTime) => {
   if (unixTime) {
-    const date = new Date(unixTime * 1000);
+    const date = new Date(unixTime);
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
