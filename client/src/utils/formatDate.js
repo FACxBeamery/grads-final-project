4e4e3c69738fec365ddf74f4eee60d6edf2fa599
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-12-02 14:29:26 +0000

    Fix format date and test

--- client/src/utils/formatDate.js
+++ client/src/utils/formatDate.js
@@ -1,14 +1,16 @@
 const formatDate = (unixTime) => {
   if (unixTime) {
-    const date = new Date(unixTime);
+    let time = unixTime;
+    if (typeof unixTime === 'string') {
+      time = Number(time);
+    }
+    const date = new Date(time);
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const ddmmyyyyDate = `${day}/${month}/${year}`;
-
     return ddmmyyyyDate;
   }
   return `input is not unix`;
 };
-
 export default formatDate;
--- client/src/utils/formatDate.test.js
+++ client/src/utils/formatDate.test.js
@@ -2,8 +2,8 @@ import formatDate from './formatDate';
 
 describe('date should format unix time to dd/mm/yyyy ', () => {
   it('should format date', () => {
-    expect(formatDate(123123123)).toEqual('26/11/1973');
-    expect(formatDate('123123123')).toEqual('26/11/1973');
+    expect(formatDate(123123123)).toEqual('2/1/1970');
+    expect(formatDate('123123123')).toEqual('2/1/1970');
     expect(formatDate('')).toEqual(`input is not unix`);
     expect(formatDate(null)).toEqual(`input is not unix`);
   });
