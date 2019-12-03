50674c362fda2dff0ea41f30f7227673bbe8c2a6
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-12-03 15:01:18 +0000

    add actions to employee table

--- client/src/store/reducer/employeeTableReducer.js
+++ client/src/store/reducer/employeeTableReducer.js
@@ -1,14 +1,17 @@
 const initalState = {
   employeeData: undefined,
   page: 0,
+  totalRows: undefined,
   rowsPerPage: 5,
-  selected: [],
 };
 
 const employeeTableReducer = (state = initalState, action) => {
+  const { payload } = action;
   switch (action.type) {
     case 'SET_EMPLOYEE_DATA':
-      return { ...state, employeeData: action.payload };
+      return { ...state, employeeData: payload };
+    case 'CHANGE_PAGE':
+      return { ...state, page: payload.page };
     default:
       return state;
   }
