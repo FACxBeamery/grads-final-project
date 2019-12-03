874e7db2d130c30eb34a237da5d8aa485beb9ca4
“joaoviana” <joaopadraoviana@gmail.com>
2019-12-03 22:09:54 +0000

    add button to header and redirect to dashboard

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
-import { AppBar, Box } from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { AppBar, Button, Box } from '@material-ui/core';
 import styles from './Header.module.css';
 
-const Header = () => {
+const Header = ({ history }) => {
   return (
-    <AppBar data-testid='app-bar' position='static'>
-      <Box className={styles['header-title']}>
-        <h2 className={styles['header-title-vibe']}>Vibe@</h2>
-        <h2 className={styles['header-title-beamery']}> Beamery</h2>
+    <AppBar data-testid='app-bar' position='relative'>
+      <h2 className={styles['header']}>
+        <span
+          onClick={() => history.push(`/admin`)}
+          className={styles['clickable']}
+        >
+          <span className={styles['header-title-vibe']}>vibe@</span>
+          <span className={styles['header-title-beamery']}>Beamery</span>
+        </span>
+      </h2>
+      <Box mr={2} className={styles['button']}>
+        <Button
+          onClick={() => history.push(`/admin`)}
+          style={{ backgroundColor: '#FFFFFF' }}
+          variant='outlined'
+        >
+          {' '}
+          My Dashboard
+        </Button>
       </Box>
     </AppBar>
   );
 };
 
-export default Header;
+Header.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  history: PropTypes.shape({ push: PropTypes.func }).isRequired,
+};
+
+export default withRouter(Header);
