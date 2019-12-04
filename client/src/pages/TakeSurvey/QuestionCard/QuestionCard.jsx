29fcce86eeb05f5892c3b8139196e8a24a3bdf49
lyndseysc <lyndsey.736@gmail.com>
2019-12-04 11:13:36 +0000

    add comment box

--- client/src/pages/TakeSurvey/QuestionCard/QuestionCard.jsx
+++ client/src/pages/TakeSurvey/QuestionCard/QuestionCard.jsx
@@ -3,6 +3,52 @@ import { Box, Button, TextField, Typography } from '@material-ui/core';
 
 import { useSelector, useDispatch } from 'react-redux';
 
+
+
+
+const CommentBox = () => {
+  const dispatch = useDispatch();
+  const activeQuestion = useSelector(
+    (state) => state.takeSurveyReducer.activeQuestion,
+  );
+  const activeStep = useSelector((state) => state.takeSurveyReducer.activeStep);
+  const currentCommentText = useSelector(
+    (state) => state.takeSurveyReducer.answers[activeStep - 1].comment,
+  );
+  const handleComment = (event) => {
+ 
+  dispatch({
+    type: 'ADD_COMMENT',
+    payload: {
+      questionId: activeQuestion.id,
+      comment: event.target.value ,
+    },
+  });
+}
+      return (
+        <Box mt={4} display='flex' flexDirection='column' alignSelf='flex-start'>
+        <Typography variant='h5'>Add a Comment (optional)</Typography>
+        <Box mt={2}>
+          <TextField
+            data-testid='comment-box'
+            autoFocus
+            fullWidth
+            required
+            autoComplete='off'
+            value={currentCommentText}
+            margin='normal'
+            key={activeStep}
+            name='comment-text-box'
+            onChange={handleComment}
+
+          />
+        </Box>
+      </Box>
+
+      )
+}
+
+
 const MultichoiceQuestionOptions = () => {
   const activeQuestion = useSelector(
     (state) => state.takeSurveyReducer.activeQuestion,
@@ -13,9 +59,11 @@ const MultichoiceQuestionOptions = () => {
     (state) => state.takeSurveyReducer.answers[activeStep - 1].answer,
   );
 
+
   const dispatch = useDispatch();
 
   const handleButtonClick = (event) => {
+
     event.preventDefault();
     dispatch({
       type: 'ENABLE_NEXT',
@@ -24,26 +72,24 @@ const MultichoiceQuestionOptions = () => {
       type: 'ADD_RESPONSE',
       payload: {
         questionId: activeQuestion.id,
-        answer: event.target.innerText,
-        comment: null,
-      },
+        answer:
+          event.target.innerText,
+      
+      }
     });
-  };
+  }
 
   return activeQuestion.options.map((option) => {
-    const buttonColor = currentAnswer
-      ? currentAnswer.toLowerCase() === option.text
-        ? '#201E5A'
-        : '#e8e8e8'
-      : '#e8e8e8';
-    const buttonTextColor = currentAnswer
-      ? currentAnswer.toLowerCase() === option.text
-        ? '#fff'
-        : 'black'
-      : 'black';
+    const isAnswerSelected = currentAnswer && currentAnswer.toLowerCase() === option.text
+    const buttonColor = isAnswerSelected && '#201E5A'
+     
+    const buttonTextColor = isAnswerSelected && '#fff'
+      
+   
 
     return (
       <Box mb={2}>
+        
         <Button
           size='large'
           style={{
@@ -59,6 +105,9 @@ const MultichoiceQuestionOptions = () => {
         >
           {option.text}
         </Button>
+     
+    
+   
       </Box>
     );
   });
@@ -68,12 +117,22 @@ const MultichoiceQuestion = () => {
   const activeQuestion = useSelector(
     (state) => state.takeSurveyReducer.activeQuestion,
   );
+  const activeStep = useSelector((state) => state.takeSurveyReducer.activeStep);
+
+  const currentAnswer = useSelector(
+    (state) => state.takeSurveyReducer.answers[activeStep - 1].answer,
+  );
+
   return (
     <Box mt={4} display='flex' flexDirection='column' alignSelf='flex-start'>
       <Typography variant='h5'>{activeQuestion.title}</Typography>
       <Box mt={2} display='flex' flexDirection='column' alignItems='center'>
-        <MultichoiceQuestionOptions />
-      </Box>
+       <MultichoiceQuestionOptions /> 
+       </Box>
+      
+        { currentAnswer && <CommentBox />}
+    
+     
     </Box>
   );
 };
