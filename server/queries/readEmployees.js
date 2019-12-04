8cd64bafe74b338ae209311fd972c449a755b52f
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-12-04 17:03:29 +0000

    employees query changed to not use pagination

--- server/queries/readEmployees.js
+++ server/queries/readEmployees.js
@@ -3,7 +3,18 @@ const { getDb } = require('../databaseConnection');
 //Using this resource https://arpitbhayani.me/blogs/benchmark-and-compare-pagination-approach-in-mongodb
 // Decided to use skip and limit approach due to the size of the data set.
 
-const readEmployees = async (pageSize, pageNumber) => {
+const readEmployees = async () => {
+  try {
+    const db = getDb();
+    const employees = db.collection('Employees');
+    const result = await employees.find({}).toArray();
+    return result;
+  } catch (err) {
+    return err;
+  }
+};
+
+const readPaginatedEmployees = async (pageSize, pageNumber) => {
   try {
     const skips = pageSize * (pageNumber - 1);
     const db = getDb();
@@ -19,4 +30,4 @@ const readEmployees = async (pageSize, pageNumber) => {
   }
 };
 
-module.exports = readEmployees;
+module.exports = { readEmployees, readPaginatedEmployees };
