fd7c0dc5f1766b3ef5dcda5f81719fda41ec2ec3
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-12-05 15:26:08 +0000

    remove header from main app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,9 +10,6 @@ const App = () => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-        <Box mb={5}>
-          <Header />
-        </Box>
         <Main />
       </ThemeProvider>
     </Provider>
