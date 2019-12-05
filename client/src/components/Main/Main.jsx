b4fcb94bb2a727a69c049b34d2cbbae4a50f9d5e
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-12-05 15:26:29 +0000

    add header on top of switch component in main

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Box } from '@material-ui/core';
+import Header from '../Header/Header';
 import AdminLogin from '../../pages/AdminLogin';
 import Dashboard from '../../pages/Dashboard';
 import CreateSurvey from '../../pages/CreateSurvey';
@@ -14,15 +15,28 @@ const Main = () => {
 
   return (
     <main>
-      <Box mx={4}>
-        <Switch>
-          <Route exact path='/admin/login' component={AdminLogin} />
-          <Route exact path='/admin' component={Dashboard} />
-          <Route exact path='/admin/surveys/create' component={CreateSurvey} />
-          <Route exact path='/admin/surveys/edit/:id' component={EditSurvey} />
-          <Route path='/admin/surveys/:id' component={SurveyDetail} />
-          <Route path='/:id' component={TakeSurvey} />
-        </Switch>
+      <Box>
+        <Box mb={5}>
+          <Header />
+        </Box>
+        <Box mx={4}>
+          <Switch>
+            <Route exact path='/admin/login' component={AdminLogin} />
+            <Route exact path='/admin' component={Dashboard} />
+            <Route
+              exact
+              path='/admin/surveys/create'
+              component={CreateSurvey}
+            />
+            <Route
+              exact
+              path='/admin/surveys/edit/:id'
+              component={EditSurvey}
+            />
+            <Route path='/admin/surveys/:id' component={SurveyDetail} />
+            <Route path='/:id' component={TakeSurvey} />
+          </Switch>
+        </Box>
       </Box>
     </main>
   );
