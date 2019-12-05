e376bdbf4c49b8498ddc63d1255d66d03f27fff1
TKostrzewski <thomas_kostrzewski@hotmail.com>
2019-12-05 20:52:45 +0000

    Show the publish and close curvey buttons selectively depending on the status of the survey

--- client/src/pages/SurveyDetail/index.jsx
+++ client/src/pages/SurveyDetail/index.jsx
@@ -3,7 +3,6 @@ import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 // import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-
 import {
   Typography,
   Button,
@@ -13,6 +12,7 @@ import {
   Step,
   StepLabel,
 } from '@material-ui/core';
+import Snackbar from '../../components/Snackbar';
 
 const getSurveyStatusForStepper = async (_id) => {
   const result = await axios.get(`/surveys/${_id}`);
@@ -20,18 +20,27 @@ const getSurveyStatusForStepper = async (_id) => {
   return surveyStatusToIndex[result.data.status];
 };
 
+const getSurveyTitle = async (_id) => {
+  const result = await axios.get(`/surveys/${_id}`);
+  return result.data.title;
+};
+
+let successfulPublish;
+let successfulClose;
 const publishSurvey = async (_id) => {
-  await axios.patch(`/surveys/${_id}`, {
+  const response = await axios.patch(`/surveys/${_id}`, {
     status: 'published',
     datePublished: Date.now(),
   });
+  successfulPublish = response.status === 204;
 };
 
 const closeSurvey = async (_id) => {
-  await axios.patch(`/surveys/${_id}`, {
+  const response = await axios.patch(`/surveys/${_id}`, {
     status: 'closed',
     dateClosed: Date.now(),
   });
+  successfulClose = response.status === 204;
 };
 
 const SurveyDetail = ({ match }) => {
@@ -40,9 +49,9 @@ const SurveyDetail = ({ match }) => {
 
   const dispatch = useDispatch();
 
-  const { title, status, datePublished, dateClosed } = useSelector(
-    (state) => state.createSurveyReducer,
-  );
+  //   const { title, status, datePublished, dateClosed } = useSelector(
+  //     (state) => state.createSurveyReducer,
+  //   );
   const { surveyStatus } = useSelector((state) => state.surveyDetailReducer);
 
   const { id } = match.params;
@@ -106,9 +115,34 @@ const SurveyDetail = ({ match }) => {
 
   return (
     <Box display='flex' flexDirection='row' align-items='flex-start'>
+      <Typography color='primary' variant='h3'>
+        Survey title.
+      </Typography>
       <StepperT />
-      <PublishSurveyButton />
-      <CloseSurveyButton />
+      {surveyStatus === 1 && <PublishSurveyButton />}
+      {surveyStatus === 2 && <CloseSurveyButton />}
+      {successfulPublish !== undefined && (
+        <Snackbar
+          message={
+            successfulPublish
+              ? 'The survey is now published and can welcome responses.'
+              : 'There was an error publishing survey. Please try again.'
+          }
+          variant={successfulPublish ? 'success' : 'error'}
+          timeopened={Date.now()}
+        />
+      )}
+      {successfulClose !== undefined && (
+        <Snackbar
+          message={
+            successfulClose
+              ? 'The survey is now closed. No more responses will be recorded.'
+              : 'There was an error closing the survey. Please try again.'
+          }
+          variant={successfulClose ? 'success' : 'error'}
+          timeopened={Date.now()}
+        />
+      )}
     </Box>
   );
 };
