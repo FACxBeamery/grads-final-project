538c92b634c605238f2119d4b38171a2e761ca9f
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-12-05 11:48:32 +0000

    move options into table filters

--- client/src/components/EmployeeTable/index.jsx
+++ client/src/components/EmployeeTable/index.jsx
@@ -11,17 +11,12 @@ import {
   TableHead,
   TableRow,
   Toolbar,
-  TextField,
   Typography,
   Checkbox,
-  FormControl,
-  FormControlLabel,
-  FormLabel,
-  FormGroup,
   TablePagination,
 } from '@material-ui/core';
+import RecipientOptions from './TableFilters/TableFilters';
 
-// TODO make searches reset when searchbar text
 const headCells = [
   {
     id: 'name',
@@ -105,104 +100,6 @@ const EnhancedTableToolbar = () => {
         <Typography variant='h5'>Employees</Typography>
       )}
     </Toolbar>
-  );
-};
-
-const OptionsCheckbox = ({ options, attribute }) => {
-  const { filters } = useSelector((state) => state.employeeTableReducer);
-  const dispatch = useDispatch();
-
-  const checkAllValuesTrue = (object) => {
-    return Object.values(object).every((element) => element);
-  };
-
-  const allChecked = checkAllValuesTrue(filters[attribute]);
-
-  const handleOptionChange = (event, option) => {
-    const payload = { checked: event.target.checked, option, attribute };
-    dispatch({ type: 'SET_FILTER_OPTION', payload });
-    dispatch({ type: 'FILTER_DATA', payload });
-  };
-
-  const handleAllFilter = (event) => {
-    const payload = { checked: event.target.checked, attribute };
-    dispatch({ type: 'SET_ALL_FILTER', payload });
-    dispatch({ type: 'FILTER_DATA', payload });
-  };
-
-  return (
-    <Box mr={4}>
-      <FormControl component='fieldset'>
-        <FormLabel component='legend'>
-          {attribute === 'department' ? 'Department' : 'Office'}
-        </FormLabel>
-        <FormGroup>
-          <Box display='flex'>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={allChecked}
-                  onChange={handleAllFilter}
-                  value={allChecked}
-                  name='select-all'
-                  inputProps={{
-                    'aria-label': 'Select all: ',
-                  }}
-                />
-              }
-              label='Select all'
-            />
-            {options.map((option) => (
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    checked={filters[attribute][option]}
-                    onChange={
-                      (event) => handleOptionChange(event, option, attribute)
-                      // eslint-disable-next-line react/jsx-curly-newline
-                    }
-                    value={option}
-                  />
-                }
-                label={option}
-              />
-            ))}
-          </Box>
-        </FormGroup>
-      </FormControl>
-    </Box>
-  );
-};
-
-const RecipientOptions = () => {
-  const { department, office, name } = useSelector(
-    (state) => state.employeeTableReducer.filters,
-  );
-  const dispatch = useDispatch();
-
-  const handleSearchbarChange = (event) => {
-    const payload = { text: event.target.value, attribute: 'name' };
-    dispatch({ type: 'SET_SEARCHBAR_TEXT', payload });
-    dispatch({ type: 'FILTER_DATA', payload });
-  };
-
-  const departmentOptions = Object.keys(department);
-  const officeOptions = Object.keys(office);
-
-  return (
-    <Box display='flex'>
-      <OptionsCheckbox options={departmentOptions} attribute='department' />
-      <OptionsCheckbox options={officeOptions} attribute='office' />
-      <TextField
-        id='name-search'
-        label='Search for a person'
-        type='search'
-        margin='normal'
-        variant='outlined'
-        value={name}
-        onChange={handleSearchbarChange}
-      />
-    </Box>
   );
 };
 
@@ -309,7 +206,7 @@ const EmployeesTable = () => {
             </Table>
           </Box>
           <TablePagination
-            rowsPerPageOptions={[10, 15, 20]}
+            rowsPerPageOptions={[5, 10, 15]}
             component='div'
             count={filteredEmployeeData.length}
             rowsPerPage={rowsPerPage}
