b07bf3008fdd6ca4e16d61a9f87b736de5aae22f
lyndseysc <lyndsey.736@gmail.com>
2019-12-06 09:16:32 +0000

    patch employees handler

---
+++ server/handlers/patchEmployeesWithSlackID.js
@@ -0,0 +1,18 @@
+const updateEmployeesWithSlackID = require('../queries/updateEmployeesWithSlackID');
+
+const patchEmployeesWithSlackID = async (req, res) => {
+  // const employeeID = req.params.employeeID
+  // const slackID = req.params.slackID
+  try {
+    const result = await updateEmployeesWithSlackID();
+    res.status(200).json(result);
+  } catch (err) {
+    res
+      .status(500)
+      .json(
+        "We're experiencing some issues on our end. Please inform the engineers and we will get back to you",
+      );
+  }
+};
+
+module.exports = patchEmployeesWithSlackID;
