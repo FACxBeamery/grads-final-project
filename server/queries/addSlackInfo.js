a0e05d61c4a59d98afd5045ec9202c212782d383
lyndseysc <lyndsey.736@gmail.com>
2019-12-06 09:16:01 +0000

    add slack info query used in update employees with slack id

--- server/queries/addSlackInfo.js
+++ server/queries/addSlackInfo.js
@@ -1,16 +1,16 @@
 // const { getDb } = require('../databaseConnection');
 const { getDb } = require('../databaseConnection');
-const ObjectID = require('mongodb').ObjectID;
 
-const addSlackInfo = async (employeeID, slackID, IMChannelID) => {
+const addSlackInfo = async (employeeEmail, slackIDToAdd) => {
   const db = getDb();
   const EmployeesCollection = await db.collection('Employees');
 
   try {
     const updateEmployee = EmployeesCollection.updateOne(
-      { _id: ObjectID(employeeID) },
-      { $push: { slackID: slackID, IMChannel: IMChannelID } },
+      { email: employeeEmail },
+      { $push: { slackID: slackIDToAdd } },
     );
+    console.log(EmployeesCollection);
     return updateEmployee;
   } catch (err) {
     return err;
