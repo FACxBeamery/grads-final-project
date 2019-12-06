aaaaef0afb3fade09398cacd8971d7f1ff20218d
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-12-06 13:19:20 +0000

    wrap createsurvey with browserrouter

--- client/src/pages/CreateSurvey/CreateSurvey.test.jsx
+++ client/src/pages/CreateSurvey/CreateSurvey.test.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom';
 import CreateSurveyForm from '.';
 import store from '../../store';
 
 describe('Form works as expected', () => {
   it('shoudl display error message if title too short', () => {
     const { getAllByRole, getByText } = render(
-      <Provider store={store}>
-        <CreateSurveyForm questionIndex={0} />
-      </Provider>,
+      <BrowserRouter>
+        <Provider store={store}>
+          <CreateSurveyForm questionIndex={0} />
+        </Provider>
+      </BrowserRouter>,
     );
 
     const inputs = getAllByRole('textbox');
