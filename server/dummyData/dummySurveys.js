d70b4070f3ff4b97d896aab8dc98dc27e77586a4
Antonio Gargaro <antoniogargaro97@gmail.com>
2019-12-10 14:58:39 +0000

    Updating to masters copy

--- server/dummyData/dummySurveys.js
+++ server/dummyData/dummySurveys.js
@@ -20,21 +20,27 @@ module.exports = [
     anonymous: false,
     recipients: [
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439014'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439012'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439013'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439015'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439016'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439017'),
         completed: false,
       },
     ],
@@ -63,48 +69,63 @@ module.exports = [
     anonymous: true,
     recipients: [
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439014'),
         completed: true,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439012'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439013'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439015'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439016'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439017'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439011'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439018'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439019'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439023'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439033'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439043'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439053'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439063'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439073'),
         completed: false,
       },
     ],
@@ -124,34 +145,43 @@ module.exports = [
         employeeId: null,
         answers: [
           {
+            questionId: ObjectID('707f1f87bcf86dd799439011'),
             answer:
               'My first week was okay. I am just writing sentences for dummy data. Here is another sentence',
             comment: 'This is my comment on this answer',
           },
           {
+            questionId: ObjectID('707f1f87bcf86dd799439021'),
             answer: 'meh',
             comment: null,
           },
           {
+            questionId: ObjectID('707f1f87bcf86dd799439121'),
             answer: 'strongly agree',
             comment: 'I am commenting on this question',
           },
           {
+            questionId: ObjectID('707f1f87bcf76dd789439121'),
             answer: 'Hi another sentence to mock data!',
           },
           {
+            questionId: ObjectID('707f1f87bcf76dd799439121'),
             answer: 'Hi another sentence to mock data! Number 2',
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439121'),
             answer: 'Hi another sentence to mock data! Number 2',
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799539126'),
             answer: 'Hi another sentence to mock data! Number 3',
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439126'),
             answer: 'Hi another sentence to mock data! Number 4',
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439127'),
             answer: 'yes',
           },
         ],
@@ -160,34 +190,43 @@ module.exports = [
         employeeId: null,
         answers: [
           {
+            questionId: ObjectID('707f1f87bcf86dd799439011'),
             answer:
               'My first week was great. I am just writing sentences for another dummy data. Here is another sentence',
             comment: 'This is my comment on this answer',
           },
           {
+            questionId: ObjectID('707f1f87bcf86dd799439021'),
             answer: 'good',
             comment: null,
           },
           {
+            questionId: ObjectID('707f1f87bcf86dd799439121'),
             answer: 'neutral',
             comment: 'I am commenting on this question',
           },
           {
+            questionId: ObjectID('707f1f87bcf76dd789439121'),
             answer: '2nd response Hi another sentence to mock data!',
           },
           {
+            questionId: ObjectID('707f1f87bcf76dd799439121'),
             answer: '2nd response Hi another sentence to mock data! Number 2',
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439121'),
             answer: '2nd response Hi another sentence to mock data! Number 2',
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799539126'),
             answer: '2nd response Hi another sentence to mock data! Number 3',
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439126'),
             answer: '2nd response Hi another sentence to mock data! Number 4',
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439127'),
             answer: 'no',
           },
         ],
@@ -211,48 +250,63 @@ module.exports = [
     anonymous: true,
     recipients: [
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439014'),
         completed: true,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439012'),
         completed: true,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439013'),
         completed: true,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439015'),
         completed: true,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439016'),
         completed: true,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439017'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439011'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439018'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439019'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439023'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439033'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439043'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439053'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439063'),
         completed: false,
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439073'),
         completed: false,
       },
     ],
@@ -264,66 +318,86 @@ module.exports = [
     ],
     responses: [
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439014'),
         answers: [
           {
+            questionId: ObjectID('507f1f77bcf86cd799439033'),
             answer: 1,
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439126'),
             answer: 'no! im okay!',
           },
           {
+            questionId: ObjectID('707f1f87bcf86dd799439021'),
             answer: 3,
           },
         ],
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439012'),
         answers: [
           {
+            questionId: ObjectID('507f1f77bcf86cd799439033'),
             answer: 0,
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439126'),
             answer: 'no! im okay!',
           },
           {
+            questionId: ObjectID('707f1f87bcf86dd799439021'),
             answer: 2,
           },
         ],
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439013'),
         answers: [
           {
+            questionId: ObjectID('507f1f77bcf86cd799439033'),
             answer: 0,
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439126'),
             answer: 'no! im okay!',
           },
           {
+            questionId: ObjectID('707f1f87bcf86dd799439021'),
             answer: 2,
           },
         ],
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439015'),
         answers: [
           {
+            questionId: ObjectID('507f1f77bcf86cd799439033'),
             answer: 0,
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439126'),
             answer: 'no! im oapsodmnaposmx aks xpioa scxkay!',
           },
           {
+            questionId: ObjectID('707f1f87bcf86dd799439021'),
             answer: 1,
           },
         ],
       },
       {
+        employeeId: ObjectID('507f1f77bcf86cd799439016'),
         answers: [
           {
+            questionId: ObjectID('507f1f77bcf86cd799439033'),
             answer: 1,
           },
           {
+            questionId: ObjectID('707f1f87bdf76dd799439126'),
             answer: 'no! imapiosdma sd ipkq wd !!! scxkay!',
           },
           {
+            questionId: ObjectID('707f1f87bcf86dd799439021'),
             answer: 4,
           },
         ],
