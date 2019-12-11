e184f4102a968b323dd39b416f055086dbca9790
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-12-11 09:18:43 +0000

    remove unecessary console log

--- server/queries/readSurveys.js
+++ server/queries/readSurveys.js
@@ -2,6 +2,8 @@ const { getDb } = require('../databaseConnection');
 const readUsers = async () => {
   try {
     const db = getDb();
+    console.log('surveys', db);
+
     const surveys = db.collection('Surveys');
     const result = await surveys.find().toArray();
     return result;
