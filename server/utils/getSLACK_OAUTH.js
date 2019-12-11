8d9f0eadc57683af9d87314d81419a94e54ddc09
lyndseysc <lyndsey.736@gmail.com>
2019-12-11 13:20:20 +0000

    file to get o auth in future

---
+++ server/utils/getSLACK_OAUTH.js
@@ -0,0 +1,14 @@
+const getSLACK_OAUTH = () => {
+  try {
+    if (!process.env.SLACK_OAUTH)
+      throw new Error('SLACK_OAUTH environment variable not configured.');
+
+    return process.env.SLACK_OAUTH;
+  } catch (err) {
+    console.error(err);
+    console.error('Exiting system with error code 1.');
+    return process.exit(1);
+  }
+};
+
+module.exports = getSLACK_OAUTH;
