96da379c9483cb068ff43a3beda5378683974edd
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-12-11 14:37:30 +0000

    display survey icon to duplicate from template on table

--- client/src/pages/SurveyBuilderFromTemplate/components/TemplateSurveysTable.jsx
+++ client/src/pages/SurveyBuilderFromTemplate/components/TemplateSurveysTable.jsx
@@ -4,8 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import {
+  Box,
   Tooltip,
-  Fab,
+  Button,
   Table,
   TableBody,
   TableCell,
@@ -13,7 +14,7 @@ import {
   TableRow,
   Paper,
 } from '@material-ui/core';
-import { FileCopy } from '@material-ui/icons';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
 
 import styles from './TemplateSurveysTable.module.css';
 
@@ -21,39 +22,36 @@ import formatDate from '../../../utils/formatDate';
 import sortArrayByObjsKey from '../../../utils/sortArrayByObjsKey';
 
 const TemplateSurveysTable = ({ history }) => {
-  const cells = [
-    '',
-    'Survey',
-    'Description',
-    'Number of questions',
-    'Responses',
-    'Date Published',
-  ];
+  const cells = ['', 'Survey', 'Description', 'Responses', 'Date Published'];
   const { surveys } = useSelector((state) => ({
     surveys: state.dashboardReducer.surveys,
   }));
 
   const dispatch = useDispatch();
 
-  const goToSurveyBuilderWithPreexistingData = async (_id) => {
+  const goToSurveyBuilderWithPreexistingData = async (id) => {
     let surveySelected;
-    // eslint-disable-next-line no-underscore-dangle
     try {
-      const { data } = await axios.get(`/surveys/${_id}`);
-      surveySelected = data;
+      const { data } = await axios.get(`/surveys/${id}`);
+      surveySelected = (({ _id, ...others }) => ({ ...others }))(data);
 
       surveySelected.questions = surveySelected.questions.map((question) => {
-        delete question._id;
-        return question;
+        return (({ _id, ...others }) => ({ ...others }))(question);
       });
       surveySelected.recipients = surveySelected.recipients.map((recipient) => {
         return { ...recipient, completed: false };
       });
-      console.log(surveySelected);
+
+      dispatch({ type: 'RESET_EMPLOYEE_DATA' });
       dispatch({
         type: 'SET_SURVEY_DATA_FROM_TEMPLATE',
         payload: surveySelected,
       });
+      dispatch({
+        type: 'SET_EMPLOYEE_TABLE_RECIPIENTS',
+        payload: data.recipients,
+      });
+
       history.push(`/admin/surveys/create`);
     } catch (error) {
       history.push('/admin/surveys');
@@ -77,50 +75,55 @@ const TemplateSurveysTable = ({ history }) => {
 
   const sortedSurveys = sortArrayByObjsKey(surveys, 'status', 'descending');
   return (
-    <Paper>
-      <Table aria-label='all surveys table' className={styles.table}>
-        <TableHead>
-          <TableRow>
-            {cells.map((cell, idx) => {
-              // eslint-disable-next-line react/no-array-index-key
-              return <TableCell key={idx}>{cell}</TableCell>;
+    <Box my={4}>
+      <Paper>
+        <Table aria-label='all surveys table' className={styles.table}>
+          <TableHead>
+            <TableRow>
+              {cells.map((cell, idx) => {
+                return (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <TableCell style={{ fontWeight: 700 }} key={idx}>
+                    {cell}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {sortedSurveys.map((survey) => {
+              const {
+                title,
+                datePublished,
+                recipients,
+                _id,
+                responses,
+                description,
+              } = survey;
+              return (
+                <TableRow key={title} className={styles.row}>
+                  <TableCell scope='row'>
+                    <Tooltip title='Duplicate From' aria-label='duplicatefrom'>
+                      <Button
+                        onClick={goToSurveyBuilderWithPreexistingData(_id)}
+                      >
+                        <FileCopyOutlinedIcon style={{ fontSize: 20 }} />
+                      </Button>
+                    </Tooltip>
+                  </TableCell>
+                  <TableCell scope='row'>{title}</TableCell>
+                  <TableCell>{description}</TableCell>
+                  <TableCell>{`${responses.length}/${recipients.length} respondents`}</TableCell>
+                  <TableCell>
+                    {formatDate(datePublished) || 'No date'}
+                  </TableCell>
+                </TableRow>
+              );
             })}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {sortedSurveys.map((survey) => {
-            const {
-              _id,
-              title,
-              datePublished,
-              recipients,
-              questions,
-              responses,
-              description,
-            } = survey;
-            return (
-              <TableRow key={title} className={styles.row}>
-                <TableCell scope='row'>
-                  <Tooltip title='Duplicate From' aria-label='duplicatefrom'>
-                    <Fab
-                      onClick={() => goToSurveyBuilderWithPreexistingData(_id)}
-                      color='secondary'
-                    >
-                      <FileCopy />
-                    </Fab>
-                  </Tooltip>
-                </TableCell>
-                <TableCell scope='row'>{title}</TableCell>
-                <TableCell>{description}</TableCell>
-                <TableCell>{questions.length}</TableCell>
-                <TableCell>{`${responses.length}/${recipients.length} respondents`}</TableCell>
-                <TableCell>{formatDate(datePublished) || 'No date'}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
+          </TableBody>
+        </Table>
+      </Paper>
+    </Box>
   );
 };
 
