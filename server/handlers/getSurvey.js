35dd62e24fcbb82094ea5a0667fcd9f7a6768915
thomas-galligan <tom.galligan@beamery.com>
2019-12-11 16:58:07 +0000

    fix getSurvey test

--- server/handlers/getSurvey.js
+++ server/handlers/getSurvey.js
@@ -7,6 +7,7 @@ const getSurvey = async (req, res) => {
     const result = await readSurvey(id.toString());
     res.status(200).json(result);
   } catch (err) {
+    console.log(err)
     res
       .status(500)
       .json(
--- server/handlers/getSurvey.test.js
+++ server/handlers/getSurvey.test.js
@@ -3,28 +3,19 @@ const request = require('supertest');
 const { initDb, closeDb } = require('../databaseConnection');
 const app = require('../app');
 
-beforeEach(() => {
-  return initDb();
-});
-
-afterEach(() => {
-  return closeDb();
-});
 
 describe('Testing GET /surveys/:id', () => {
-  it('Responds with JSON and status 200 when the survey has been found successfully.', (done) => {
-    const documentIdToFind = '508f1f99bcf86cd799439014';
-    request(app)
-      .get(`/surveys/${documentIdToFind}`)
+  it('Responds with JSON and status 200 when the survey has been found successfully.', async () => {
+    // const documentIdToFind = '508f1f99bcf86cd799439014';
+
+    await initDb();
+
+    const res = await request(app)
+      .get('/surveys/508f1f99bcf86cd799439014')
       .set('Accept', 'application/json')
-      .expect(200)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        expect(res.body.auth).toEqual(true);
-        return done();
-      });
-    done();
+      .expect('Content-Type', /json/)
+
+    expect(res.status).toEqual(200)
+    return closeDb();
   });
 });
