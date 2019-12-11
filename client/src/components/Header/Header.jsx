c5bc5a58bd87c7f1db358cdd3b26927c0f2f5218
lyndseysc <lyndsey.736@gmail.com>
2019-12-11 16:45:35 +0000

    delete space for joão

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -25,7 +25,6 @@ const Header = ({ history, match }) => {
             onClick={() => history.push(`/admin`)}
             style={{ color: '#FFFFFF' }}
           >
-            {' '}
             My Dashboard
           </Button>
         </Box>
