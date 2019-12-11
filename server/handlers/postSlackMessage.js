6dc4e6020f14693990e8fdee8dd44c24da420d96
lyndseysc <lyndsey.736@gmail.com>
2019-12-11 13:20:44 +0000

    add slack handler

---
+++ server/handlers/postSlackMessage.js
@@ -0,0 +1,20 @@
+const sendSlackMessage = require('../utils/sendSlackMessage');
+
+const postSlackMessage = async (req, res) => {
+  try {
+    const slackID = req.body.slackID;
+
+    const message = req.body.message;
+
+    const result = await sendSlackMessage(slackID, message);
+    res.status(204).json(result);
+  } catch (err) {
+    res
+      .status(500)
+      .json(
+        "We're experiencing some issues on our end. Please inform the engineers and we will get back to you",
+      );
+  }
+};
+
+module.exports = postSlackMessage;
