335fa43c9af671f9f5761b79191338f1ea9fc665
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-12-11 16:30:33 +0000

    add specific db functionality to handler

--- server/handlers/protected/downloadCSV.js
+++ server/handlers/protected/downloadCSV.js
@@ -1,37 +1,48 @@
 const passport = require('passport');
 const { Parser } = require('json2csv');
 const fs = require('fs');
+const readSurvey = require('../../queries/readSurvey');
 
 const downloadCSV = async (req, res, next) => {
-  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+  passport.authenticate('jwt', { session: false }, async (err, user, info) => {
     if (err) {
+      console.log('not authenticated');
       throw new Error(err);
     }
     if (info) {
       res.status(401).json({ message: info.message });
     } else if (user.username === req.query.username) {
+      console.log('authenticated');
       if (user) {
-        const fields = ['car', 'price', 'color'];
-        const myCars = [
-          {
-            car: 'Audi',
-            price: 40000,
-            color: ['blue', 'white'],
-          },
-        ];
+        try {
+          const id = !req.params.id ? req.params : req.params.id;
+          const survey = await readSurvey(id.toString());
+          const responses = survey.responses.map(
+            (response) => response.answers,
+          );
+          console.log(responses);
 
-        const json2csvParser = new Parser({ fields });
-        const csv = json2csvParser.parse(myCars);
+          const arrayOfSizeResponses = new Array(responses.length);
+          const fields = arrayOfSizeResponses.map(
+            (item, index) => `Question ${index + 1}`,
+          );
 
-        console.log(csv);
+          const json2csvParser = new Parser({ fields });
+          const csv = json2csvParser.parse(responses);
 
-        const file = `${__dirname}/../../temp/sample2return.csv`;
+          console.log(csv);
 
-        fs.writeFileSync(file, csv);
+          const file = `${__dirname}/../../temp/sample2return.csv`;
 
-        res.download(file, () =>
-          fs.unlinkSync(file, (err2) => console.error(err2)),
-        );
+          fs.writeFileSync(file, csv);
+
+          res.download(file, () =>
+            fs.unlinkSync(file, (err2) => console.error(err2)),
+          );
+        } catch (err) {
+          console.log('in catch');
+          // do this
+        }
       } else {
         res
           .status(401)
