ba4a81e11265c40585d459dad645e988538ef107
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-12-12 15:22:11 +0000

    fix eslint

--- server/handlers/getAdmins.test.js
+++ server/handlers/getAdmins.test.js
@@ -6,57 +6,44 @@ const app = require('../app');
 describe('Test authentication using JWT tokens', () => {
   it('sends an error status if no auth token sent', async () => {
     await initDb();
-
     const res = await request(app).get('/admins');
     expect(res.status).toEqual(401);
-
     return closeDb();
   });
-
   it('sends response data if auth token sent', async () => {
     await initDb();
-
     const loginResponse = await request(app)
       .post('/login')
       .send({
         username: 'admin',
         password: 'admin',
       });
-
     expect(loginResponse.status).toEqual(200);
     expect(loginResponse.body.auth).toEqual(true);
     expect(loginResponse.body.token).toBeDefined();
-
     const {
       body: { token },
     } = loginResponse;
-
     const res = await request(app)
       .get('/admins')
       .set('Authorization', `JWT ${token}`);
-
     expect(res.status).toEqual(200);
     expect(JSON.parse(res.text)).toStrictEqual({
       message: 'Protected route accessed!',
     });
-
     return closeDb();
   });
-
   it('Responds with message and status 401 when no JWT provided.', async () => {
     const res = await request(app)
       .get('/admins')
       .set('Accept', 'application/json');
-
     expect(res.status).toEqual(401);
     expect(JSON.parse(res.text)).toStrictEqual({ message: 'No auth token' });
   });
-
   it('Empty JWT token', async () => {
     const res = await request(app)
       .get('/admins')
       .set('Authorization', `JWT `);
-
     expect(res.status).toEqual(401);
     expect(JSON.parse(res.text)).toStrictEqual({ message: 'No auth token' });
   });
