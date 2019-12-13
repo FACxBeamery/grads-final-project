c9e4e79839b3997d550b349c5a6ad41d17cc0f9b
marthaklambert <marthaklambert@Marthas-MacBook-Pro.local>
2019-12-13 14:52:19 +0000

    fix test

--- server/handlers/protected/downloadCSV.test.js
+++ server/handlers/protected/downloadCSV.test.js
@@ -3,29 +3,29 @@ const request = require('supertest');
 const { initDb, closeDb } = require('../../databaseConnection');
 const app = require('../../app');
 
-const authenticate = async () => {
-  const loginResponse = await request(app)
-    .post('/login')
-    .send({
-      username: 'admin',
-      password: 'admin',
-    });
-
-  const {
-    body: { token },
-  } = loginResponse;
-
-  return token;
-};
-beforeEach(() => {
-  return initDb();
-});
-
-afterEach(() => {
-  return closeDb();
-});
-
 describe('Testing GET /download/:id/:anonymous', () => {
+  const authenticate = async () => {
+    const loginResponse = await request(app)
+      .post('/login')
+      .send({
+        username: 'admin',
+        password: 'admin',
+      });
+
+    const {
+      body: { token },
+    } = loginResponse;
+
+    return token;
+  };
+  beforeEach(() => {
+    return initDb();
+  });
+
+  afterEach(() => {
+    return closeDb();
+  });
+
   it('Responds with status 204 when the survey has been updated successfully.', async (done) => {
     try {
       const surveyIdToDownload = '509f1f99bcf86cd799439215';
