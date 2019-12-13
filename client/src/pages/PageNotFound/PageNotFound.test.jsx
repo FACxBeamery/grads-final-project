38e8ebde27fcfff115153f3cf5fb0f1512763299
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-12-13 12:07:13 +0000

    added test cases

--- client/src/pages/PageNotFound/PageNotFound.test.jsx
+++ client/src/pages/PageNotFound/PageNotFound.test.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from 'react-router-dom';
 
 import PageNotFound from '.';
 
 it('Testing the default route page', () => {
-  const { getByText } = render(<PageNotFound />);
+  const { getByText } = render(
+    <BrowserRouter>
+      <PageNotFound />
+    </BrowserRouter>,
+  );
 
   expect(getByText('Sorry, that page doesn’t exist!')).toBeVisible();
 
-  expect(getByText('Are you an admin?')).toBeVisible();
+  expect(getByText('Taking a Survey?')).toBeVisible();
+  expect(getByText('Check if you got the right link')).toBeVisible();
+  expect(getByText('Are you an Admin?')).toBeVisible();
 
   const goToDashboardButton = getByText('Go to Dashboard');
   expect(goToDashboardButton).toBeVisible();
