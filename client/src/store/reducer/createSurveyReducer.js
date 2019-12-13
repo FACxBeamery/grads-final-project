4c196ce603ae6a955813031c76de423373617478
lyndseysc <lyndsey.736@gmail.com>
2019-12-13 11:20:44 +0000

    add link

--- client/src/store/reducer/createSurveyReducer.js
+++ client/src/store/reducer/createSurveyReducer.js
@@ -13,7 +13,7 @@ const initalState = {
   description: undefined,
   recipients: [],
   disclaimer:
-    'We respect your trust and protect your privacy by never sharing this data with third parties as well as ensuring this data will be stored until the research period is over. By filling out this survey, you agree that we will process your data in line with the Beamery Privacy Policy. If you would like to chat about this policy, or if you subsequently change your mind and want us to delete your personal data, please email steven.bianchi@beamery.com.',
+    'We respect your trust and protect your privacy by never sharing this data with third parties as well as ensuring this data will be stored until the research period is over. By filling out this survey, you agree that we will process your data in line with the Beamery Privacy Policy (www.beamery.com/privacy). If you would like to chat about this policy, or if you subsequently change your mind and want us to delete your personal data, please email steven.bianchi@beamery.com.',
   anonymous: false,
   questions: [defaultQuestion],
   openModal: false,
@@ -98,7 +98,7 @@ const createSurveyReducer = (state = initalState, action) => {
         description: undefined,
         recipients: [],
         disclaimer:
-          'We respect your trust and protect your privacy by never sharing this data with third parties as well as ensuring this data will be stored until the research period is over. By filling out this survey, you agree that we will process your data in line with the Beamery Privacy Policy. If you would like to chat about this policy, or if you subsequently change your mind and want us to delete your personal data, please email steven.bianchi@beamery.com.',
+          'We respect your trust and protect your privacy by never sharing this data with third parties as well as ensuring this data will be stored until the research period is over. By filling out this survey, you agree that we will process your data in line with the Beamery Privacy Policy (www.beamery.com/privacy). If you would like to chat about this policy, or if you subsequently change your mind and want us to delete your personal data, please email steven.bianchi@beamery.com.',
         anonymous: false,
         questions: [
           {
