4e00939e5333712b82d561bd4bc70c2477a6af17
thomas-galligan <tom.galligan@beamery.com>
2019-12-13 14:26:47 +0000

    remove explicit return from AlreadyCompletedMessage

--- client/src/pages/TakeSurvey/AlreadyCompletedMessage/AlreadyCompletedMessage.jsx
+++ client/src/pages/TakeSurvey/AlreadyCompletedMessage/AlreadyCompletedMessage.jsx
@@ -2,9 +2,6 @@ import React from "react"
 
 import { Typography } from '@material-ui/core'
 
-const AlreadyCompletedMessage = () => {
-  return <Typography variant='h3'>You have completed this survey! Thank you for your response.</Typography>
-
-}
+const AlreadyCompletedMessage = () => <Typography variant='h3'>You have completed this survey! Thank you for your response.</Typography>
 
 export default AlreadyCompletedMessage
