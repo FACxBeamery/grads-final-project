6b2309b28b10b2e056eaa71cd40ce54f2cb1880f
João Viana <joaoviana@Joaos-MacBook-Pro.local>
2019-12-13 10:25:57 +0000

    refresh errors, swap errors' indices and set helper text in options and questions

--- client/src/store/reducer/errorsBagReducer.js
+++ client/src/store/reducer/errorsBagReducer.js
@@ -1,5 +1,26 @@
 const initialState = {};
 
+const swapKeyValuesInObject = (obj, key1, key2) => {
+  console.log('KEY1', key1);
+  console.log('KEY2', key2);
+  console.log('OBJECT', obj);
+
+  [obj[key1], obj[key2]] = [obj[key2], obj[key1]];
+  return obj;
+};
+
+const returnErrorsBagWithoutDeletedProperty = (state, key) => {
+  if (state[key]) {
+    delete state[key];
+    return state;
+  }
+  return state;
+};
+
+const returnErrorsBagWithTwoSwappedValues = (state, indices) => {
+  return swapKeyValuesInObject(state, indices[0], indices[1]);
+};
+
 const errorsBagReducer = (state = initialState, action) => {
   const { payload } = action;
   switch (action.type) {
@@ -7,6 +28,10 @@ const errorsBagReducer = (state = initialState, action) => {
       return {};
     case 'ADD_ERROR':
       return { ...state, ...payload };
+    case 'REMOVE_ERROR':
+      return returnErrorsBagWithoutDeletedProperty(state, payload);
+    case 'SWAP_ERRORS':
+      return returnErrorsBagWithTwoSwappedValues(state, payload);
     default:
       return state;
   }
