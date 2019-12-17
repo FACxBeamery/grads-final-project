db31db17154985e11e72d71fd44f83b5a75e52b6
TKostrzewski <thomas_kostrzewski@hotmail.com>
2019-12-17 11:34:05 +0000

    add REACT_APP_ prefix to env variables

--- client/src/utils/addServerUrlToAxiosRequests.js
+++ client/src/utils/addServerUrlToAxiosRequests.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 axios.interceptors.request.use(
   function(config) {
-    const serverUrl = process.env.SERVER_URL;
+    const serverUrl = process.env.REACT_APP_SERVER_URL;
     config.url = join(serverUrl, config.url);
     return config;
   },
