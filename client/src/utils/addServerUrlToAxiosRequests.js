acc4d7b69da280a1f898798086a7179bbfc9cadd
lyndseysc <lyndsey.736@gmail.com>
2019-12-18 11:25:57 +0000

    fix login issue

--- client/src/utils/addServerUrlToAxiosRequests.js
+++ client/src/utils/addServerUrlToAxiosRequests.js
@@ -4,13 +4,15 @@ import join from 'url-join';
 
 import axios from 'axios';
 
-axios.interceptors.request.use(
-  function(config) {
-    const serverUrl = process.env.REACT_APP_SERVER_URL;
-    config.url = join(serverUrl, config.url);
-    return config;
-  },
-  function(error) {
-    return Promise.reject(error);
-  },
-);
+if (process.env.NODE_ENV === 'production') {
+  axios.interceptors.request.use(
+    function(config) {
+      const serverUrl = process.env.REACT_APP_SERVER_URL;
+      config.url = join(serverUrl, config.url);
+      return config;
+    },
+    function(error) {
+      return Promise.reject(error);
+    },
+  );
+}
