04659001049999348fc9fc04ba11fb0d4d68dadc
lyndseysc <lyndsey.736@gmail.com>
2019-12-18 11:34:04 +0000

    revert changes

--- client/src/utils/addServerUrlToAxiosRequests.js
+++ client/src/utils/addServerUrlToAxiosRequests.js
@@ -4,15 +4,13 @@ import join from 'url-join';
 
 import axios from 'axios';
 
-if (process.env.NODE_ENV === 'production') {
-  axios.interceptors.request.use(
-    function(config) {
-      const serverUrl = process.env.REACT_APP_SERVER_URL;
-      config.url = join(serverUrl, config.url);
-      return config;
-    },
-    function(error) {
-      return Promise.reject(error);
-    },
-  );
-}
+axios.interceptors.request.use(
+  function(config) {
+    const serverUrl = process.env.REACT_APP_SERVER_URL;
+    config.url = join(serverUrl, config.url);
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  },
+);
